45f60c7a86a489b39ea4c0f3e560a04b95afe5af
Christer Hadland <Christer.Hadland@gmail.com>
2017-01-20 15:32:26 +0100

    Added reader.pty for python Notebook

--- Code/2017 - 01 - 09 - Generating New Baby Names using RNNs.py
+++ Code/2017 - 01 - 09 - Generating New Baby Names using RNNs.py
@@ -71,9 +71,42 @@ print idx_to_vocab
 print vocab_to_idx
 print data
 
+def ptb_iterator(raw_data, batch_size, num_steps):
+  """Iterate on the raw PTB data.
+  This generates batch_size pointers into the raw PTB data, and allows
+  minibatch iteration along these pointers.
+  Args:
+    raw_data: one of the raw data outputs from ptb_raw_data.
+    batch_size: int, the batch size.
+    num_steps: int, the number of unrolls.
+  Yields:
+    Pairs of the batched data, each a matrix of shape [batch_size, num_steps].
+    The second element of the tuple is the same data time-shifted to the
+    right by one.
+  Raises:
+    ValueError: if batch_size or num_steps are too high.
+  """
+  raw_data = np.array(raw_data, dtype=np.int32)
+
+  data_len = len(raw_data)
+  batch_len = data_len // batch_size
+  data = np.zeros([batch_size, batch_len], dtype=np.int32)
+  for i in range(batch_size):
+    data[i] = raw_data[batch_len * i:batch_len * (i + 1)]
+
+  epoch_size = (batch_len - 1) // num_steps
+
+  if epoch_size == 0:
+    raise ValueError("epoch_size == 0, decrease batch_size or num_steps")
+
+  for i in range(epoch_size):
+    x = data[:, i*num_steps:(i+1)*num_steps]
+    y = data[:, i*num_steps+1:(i+1)*num_steps+1]
+    yield (x, y)
+
 def gen_epochs(n, num_steps, batch_size):
     for i in range(n):
-        yield reader.ptb_iterator(data, batch_size, num_steps)
+        yield ptb_iterator(data, batch_size, num_steps)
 
 
 def test_epoch_function(n=1, num_steps=5, batch_size=32):
